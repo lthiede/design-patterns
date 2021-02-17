59972db2f8d0526f8f049d95878985b211d5a695
limagoHub <limagoHub@limago.de>
2021-02-17 09:09:30 +0100

    mit messung

--- Tag1_01Dekorator/src/de/application/Application.java
+++ Tag1_01Dekorator/src/de/application/Application.java
@@ -1,5 +1,8 @@
 package de.application;
 
+import java.time.Duration;
+import java.time.Instant;
+
 import de.client.CalcClient;
 import de.math.Calculator;
 import de.math.CalculatorFactory;
@@ -14,6 +17,18 @@ public class Application {
 
 	public static void main(String[] args) {
 
+		
+		Instant start = Instant.now();
+		
+		// zu messende Operation
+		
+		
+		Instant ende = Instant.now();
+		
+		Duration duration = Duration.between(start, ende);
+		System.out.println(duration.toMillis());
+		
+		
 		
 		Calculator calculator = CalculatorFactory.create();
 		
--- Tag1_01Dekorator/src/de/math/CalculatorImpl.java
+++ Tag1_01Dekorator/src/de/math/CalculatorImpl.java
@@ -1,9 +1,17 @@
 package de.math;
-
+import static java.lang.Thread.sleep;
 public class CalculatorImpl implements Calculator {
 	
 	@Override
 	public double add(double a, double b) {
+		
+		try {
+			sleep(100);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return a + b;
 	}
 	@Override
