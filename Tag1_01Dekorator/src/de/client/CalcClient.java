f0c06f5f77fcfaf8c115eabdc891e696d05112bd
limagoHub <limagoHub@limago.de>
2021-02-17 08:11:38 +0100

    Initial commit

---
+++ Tag1_01Dekorator/src/de/client/CalcClient.java
@@ -0,0 +1,13 @@
+package de.client;
+
+import de.math.Calculator;
+
+public class CalcClient {
+
+	public static void main(String[] args) {
+		Calculator calc = new Calculator();
+		System.out.println(calc.add(3, 4));
+
+	}
+
+}
---
+++ Tag1_01Dekorator/src/de/math/Calculator.java
@@ -0,0 +1,12 @@
+package de.math;
+
+public class Calculator {
+	
+	public double add(double a, double b) {
+		return a + b;
+	}
+	public double sub(double a, double b) {
+		return a - b;
+	}
+
+}
